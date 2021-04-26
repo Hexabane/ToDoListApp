82fa3a4bd25887a63a02b91b17517548cde71c89
Kunal Goyal <hexabaneofficial@gmail.com>
2021-04-26 15:42:27 +0200

    New Changes

--- EditLists.Designer.cs
+++ EditLists.Designer.cs
@@ -37,46 +37,51 @@ namespace ToDoListApp
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lbViewList
             // 
             this.lbViewList.FormattingEnabled = true;
-            this.lbViewList.ItemHeight = 16;
-            this.lbViewList.Location = new System.Drawing.Point(38, 86);
+            this.lbViewList.ItemHeight = 20;
+            this.lbViewList.Location = new System.Drawing.Point(43, 108);
+            this.lbViewList.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.lbViewList.Name = "lbViewList";
-            this.lbViewList.Size = new System.Drawing.Size(189, 388);
+            this.lbViewList.Size = new System.Drawing.Size(212, 484);
             this.lbViewList.TabIndex = 0;
             this.lbViewList.SelectedIndexChanged += new System.EventHandler(this.lbViewList_SelectedIndexChanged);
             // 
             // tbEventType
             // 
-            this.tbEventType.Location = new System.Drawing.Point(574, 210);
+            this.tbEventType.Location = new System.Drawing.Point(646, 262);
+            this.tbEventType.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.tbEventType.Name = "tbEventType";
-            this.tbEventType.Size = new System.Drawing.Size(200, 22);
+            this.tbEventType.Size = new System.Drawing.Size(224, 26);
             this.tbEventType.TabIndex = 13;
             // 
             // tbEventName
             // 
-            this.tbEventName.Location = new System.Drawing.Point(574, 139);
+            this.tbEventName.Location = new System.Drawing.Point(646, 174);
+            this.tbEventName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.tbEventName.Name = "tbEventName";
-            this.tbEventName.Size = new System.Drawing.Size(200, 22);
+            this.tbEventName.Size = new System.Drawing.Size(224, 26);
             this.tbEventName.TabIndex = 12;
             // 
             // dtpWhen
             // 
-            this.dtpWhen.Location = new System.Drawing.Point(574, 281);
+            this.dtpWhen.Location = new System.Drawing.Point(646, 351);
+            this.dtpWhen.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dtpWhen.Name = "dtpWhen";
-            this.dtpWhen.Size = new System.Drawing.Size(200, 22);
+            this.dtpWhen.Size = new System.Drawing.Size(224, 26);
             this.dtpWhen.TabIndex = 11;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(291, 283);
+            this.label4.Location = new System.Drawing.Point(327, 354);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(130, 20);
+            this.label4.Size = new System.Drawing.Size(165, 25);
             this.label4.TabIndex = 10;
             this.label4.Text = "Do It by When : ";
             // 
@@ -84,9 +89,9 @@ namespace ToDoListApp
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(291, 210);
+            this.label3.Location = new System.Drawing.Point(327, 262);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(189, 20);
+            this.label3.Size = new System.Drawing.Size(242, 25);
             this.label3.TabIndex = 9;
             this.label3.Text = "Type Of Activity/Event : ";
             // 
@@ -94,27 +99,39 @@ namespace ToDoListApp
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(291, 139);
+            this.label2.Location = new System.Drawing.Point(327, 174);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(179, 20);
+            this.label2.Size = new System.Drawing.Size(228, 25);
             this.label2.TabIndex = 8;
             this.label2.Text = "Acttivity/Event Name : ";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(453, 381);
+            this.button1.Location = new System.Drawing.Point(510, 476);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(169, 58);
+            this.button1.Size = new System.Drawing.Size(190, 72);
             this.button1.TabIndex = 14;
             this.button1.Text = "EDIT LIST";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(298, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(342, 65);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "EDIT LISTS";
+            // 
             // EditLists
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(857, 519);
+            this.ClientSize = new System.Drawing.Size(964, 649);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.tbEventType);
             this.Controls.Add(this.tbEventName);
@@ -123,6 +140,7 @@ namespace ToDoListApp
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.lbViewList);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "EditLists";
             this.Text = "EditLists";
             this.ResumeLayout(false);
@@ -140,5 +158,6 @@ namespace ToDoListApp
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- ViewList.Designer.cs
+++ ViewList.Designer.cs
@@ -32,22 +32,25 @@ namespace ToDoListApp
             this.lbListNames = new System.Windows.Forms.ListBox();
             this.buttonProcess = new System.Windows.Forms.Button();
             this.lbData = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lbListNames
             // 
             this.lbListNames.FormattingEnabled = true;
-            this.lbListNames.ItemHeight = 16;
-            this.lbListNames.Location = new System.Drawing.Point(12, 113);
+            this.lbListNames.ItemHeight = 20;
+            this.lbListNames.Location = new System.Drawing.Point(14, 141);
+            this.lbListNames.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.lbListNames.Name = "lbListNames";
-            this.lbListNames.Size = new System.Drawing.Size(255, 308);
+            this.lbListNames.Size = new System.Drawing.Size(286, 384);
             this.lbListNames.TabIndex = 0;
             // 
             // buttonProcess
             // 
-            this.buttonProcess.Location = new System.Drawing.Point(306, 223);
+            this.buttonProcess.Location = new System.Drawing.Point(344, 279);
+            this.buttonProcess.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.buttonProcess.Name = "buttonProcess";
-            this.buttonProcess.Size = new System.Drawing.Size(121, 36);
+            this.buttonProcess.Size = new System.Drawing.Size(136, 45);
             this.buttonProcess.TabIndex = 1;
             this.buttonProcess.Text = ">>";
             this.buttonProcess.UseVisualStyleBackColor = true;
@@ -56,23 +59,37 @@ namespace ToDoListApp
             // lbData
             // 
             this.lbData.FormattingEnabled = true;
-            this.lbData.ItemHeight = 16;
-            this.lbData.Location = new System.Drawing.Point(479, 113);
+            this.lbData.ItemHeight = 20;
+            this.lbData.Location = new System.Drawing.Point(539, 141);
+            this.lbData.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.lbData.Name = "lbData";
-            this.lbData.Size = new System.Drawing.Size(271, 308);
+            this.lbData.Size = new System.Drawing.Size(304, 384);
             this.lbData.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(221, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(358, 65);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "VIEW LISTS";
             // 
             // ViewList
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(814, 532);
+            this.ClientSize = new System.Drawing.Size(916, 665);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.lbData);
             this.Controls.Add(this.buttonProcess);
             this.Controls.Add(this.lbListNames);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "ViewList";
             this.Text = "ViewList";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -81,5 +98,6 @@ namespace ToDoListApp
         private System.Windows.Forms.ListBox lbListNames;
         private System.Windows.Forms.Button buttonProcess;
         private System.Windows.Forms.ListBox lbData;
+        private System.Windows.Forms.Label label1;
     }
 }
